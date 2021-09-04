f57c169b17cbe950d7cad80f3f74d9fef5188a6b
Giovanni Aceto <Giovannijaceto@outlook.com>
2021-09-04 09:18:17 -0400

    Add footer html

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,8 +2,87 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>
-
+    <div className="footer">
+      <div className="learn-more">
+        <h3>Learn more</h3>
+        <button>What is Self?</button>
+        <button>Our Story</button>
+        <button>Pricing</button>
+        <button>Trust &#38; Security</button>
+        <button>Self Visa Credit Card</button>
+        <button>Help</button>
+        <button>Reviews</button>
+        <button>How to build credit</button>
+      </div>
+      <div className="about">
+        <h3>About</h3>
+        <button>About us</button>
+        <button>Careers</button>
+        <button>Investors</button>
+        <button>Press</button>
+        <button>Contact</button>
+        <button>Referrals</button>
+        <button>Site map</button>
+        <button>Affiliate</button>
+      </div>
+      <div className="social">
+        <button>Join Self</button>
+        <button>Log in</button>
+      </div>
+      <div className="terms">
+        <p>
+          <button>&copy; Self Financial, Inc.</button>
+          <button>Privacy</button>&nbsp;|&nbsp;
+          <button>Terms</button>&nbsp;|&nbsp;
+          <button>Compliance</button>&nbsp;|
+          <button>Card Agreements</button>
+        </p>
+        <p>
+          ^ Sample products: A loan with a $25 month payment, 24 month term with a $9 admin fee at a 15.92%
+          Annual Percentage Rate with a finance charge of $89; A loan with a $35 month payment, 24 month
+          term with a $9 admin fee at a 15.97% Annual Percentage Rate with a finance charge of $125; A loan
+          with a $48 month payment, 12 month term with a $9 admin fee at a 15.65% Annual Percentage Rate
+          with a finance charge of $46; A loan with a $150 month payment, 12 month term with a $9 admin fee
+          at a 15.91% Annual Percentage Rate with a finance charge of $146. Please refer to
+          www.self.inc/pricing for the most recent pricing options.
+        </p>
+        <p>
+          † Testimonial Disclaimer: Review provided by customer to Self Financial, Inc. d/b/a Self or Self
+          Lender directly. The comment above is related to individual experiences or results. Individual
+          results may vary.
+        </p>
+        <p>
+          ^^^ Source: FICO®
+        </p>
+        <p>
+          Google Play and the Google Play logo are trademarks of Google LLC.
+        </p>
+        <p>
+          App Store, Apple and the Apple logo are trademarks of Apple Inc.
+        </p>
+        <p>
+          All Credit Builder Accounts made by Lead Bank, Member FDIC, Equal Housing Lender, Sunrise Banks,
+          N.A. Member FDIC, Equal Housing Lender or Atlantic Capital Bank, N.A. Member FDIC, Equal Housing
+          Lender. Subject to ID Verification. Individual borrowers must be a U.S. Citizen or permanent
+          resident and at least 18 years old. Valid bank account and Social Security Number are required.
+          All loans are subject to ID verification and consumer report review and approval. Results are not
+          guaranteed. Improvement in your credit score is dependent on your specific situation and
+          financial behavior. Failure to make monthly minimum payments by the payment due date each month
+          may result in delinquent payment reporting to credit bureaus which may negatively impact your
+          credit score. This product will not remove negative credit history from your credit report. All
+          loans subject to approval. All Certificates of Deposit (CD) are deposited in Lead Bank, Member
+          FDIC, Sunrise Banks, N.A., Member FDIC or Atlantic Capital Bank, N.A., Member FDIC.
+        </p>
+        <p>
+          Lead Bank. Member FDIC, Equal Housing Lender
+          Sunrise Banks, N.A. Member FDIC, Equal Housing Lender
+          Atlantic Capital Bank, N.A. Member FDIC, Equal Housing Lender
+        </p>
+        <p>
+          The Self Visa® Credit Card is issued by Lead Bank, Member FDIC, Equal Housing Lender or Atlantic
+          Capital Bank, N.A., Member FDIC, Equal Housing Lender.
+        </p>
+      </div>
     </div>
   )
 }
